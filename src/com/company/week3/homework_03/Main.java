17014f1dc11f74a69bd8e6458aaeec224c9b0740
Ruzanna Zohrabyan <ruzanna83@gmail.com>
2021-07-26 16:22:10 +0400

    Correcting Week 3 Homework code

--- src/com/company/week3/homework_03/Main.java
+++ src/com/company/week3/homework_03/Main.java
@@ -4,8 +4,8 @@ public class Main {
     public static void main(String[] args) {
         LinkedIntBuffer linkedIntBuffer = new LinkedIntBuffer();
         linkedIntBuffer.add(10);
-        linkedIntBuffer.add(20);
+        linkedIntBuffer.add(400);
         linkedIntBuffer.add(30);
-        System.out.println(linkedIntBuffer.toString());
+        System.out.println(linkedIntBuffer);
     }
 }
