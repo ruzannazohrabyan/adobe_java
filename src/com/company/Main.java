fe1201ffbd86e19690921ba9fc13bd280d11411e
Ruzanna Zohrabyan <ruzanna83@gmail.com>
2021-08-21 11:45:13 +0400

    Classwork: BubbleSort Implementation

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,15 +1,8 @@
 package com.company;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class Main {
 
     public static void main(String[] args) {
-        Map<String, String> hashMap = new HashMap();
-        hashMap.put("Ruzanna", "ruzanna83@gmail.com");
-
-        System.out.println(hashMap.get("Ruzanna"));
 
     }
 }
