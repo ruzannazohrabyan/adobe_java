aa9c2b84ebcb68c319eb8d27aab04826ad9e3a52
Ruzanna Zohrabyan <ruzanna83@gmail.com>
2021-08-28 14:56:14 +0400

    Week 5: Task 1

--- src/com/company/week5/homework/task1/CustomArrayList.java
+++ src/com/company/week5/homework/task1/CustomArrayList.java
@@ -2,7 +2,7 @@ package com.company.week5.homework.task1;
 
 public class CustomArrayList<T extends Comparable<T>> {
 
-    private final int DEFAULT_CAPACITY = 3;
+    private final int DEFAULT_CAPACITY = 10;
     private final int MAX_CAPACITY = 1000;
     private T[] array;
     private int size = 0;
@@ -53,7 +53,7 @@ public class CustomArrayList<T extends Comparable<T>> {
     public T deleteByIndex(int index) {
         checkIndexValidation(index);
         T[] temp = (T[]) new Comparable[size - 1];
-        T tempElement = array[index];
+        T deletedElement = array[index];
         for (int i = 0; i < size; i++) {
             if (i < index) {
                 temp[i] = array[i];
@@ -63,7 +63,7 @@ public class CustomArrayList<T extends Comparable<T>> {
         }
         size--;
         array = temp;
-        return tempElement;
+        return deletedElement;
     }
 
     public int delete(T element) {
@@ -84,12 +84,26 @@ public class CustomArrayList<T extends Comparable<T>> {
     }
 
     public void bubbleSort() {
-        for (int i = 0; i < size-1; i++) {
+        for (int i = 0; i < size - 1; i++) {
             for (int j = 0; j < size - i - 1; j++) {
                 if (array[j].compareTo(array[j + 1]) > 0) {
                     T temp = array[j];
                     array[j] = array[j + 1];
                     array[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public void insertionSort() {
+        for (int i = 1; i < size; i++) {
+            for (int j = i; j >= 1; j--) {
+                if (array[j].compareTo(array[j-1]) < 0) {
+                    T temp = array[j];
+                    array[j] = array[j-1];
+                    array[j-1] = temp;
+                } else {
+                    break;
                 }
             }
         }
@@ -112,7 +126,7 @@ public class CustomArrayList<T extends Comparable<T>> {
     }
 
     private void checkIndexValidation(int index) {
-        if (index >= size || index < -1 || index >= MAX_CAPACITY) {
+        if (index >= size || index < 0 || index >= MAX_CAPACITY) {
             throw new IndexOutOfBoundsException(index + " is out of bound");
         }
     }
--- src/com/company/week5/homework/task1/Main.java
+++ src/com/company/week5/homework/task1/Main.java
@@ -1,26 +1,64 @@
 package com.company.week5.homework.task1;
 
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
+        Random rd = new Random();
+        CustomArrayList<Integer> customArray1 = new CustomArrayList<>(500);
+        CustomArrayList<Integer> customArray2 = new CustomArrayList<>(500);
 
-        CustomArrayList<Integer> customArray = new CustomArrayList<>();
-        customArray.add(10);
-        customArray.add(20);
-        customArray.add(0, 30);
-        customArray.add(40);
-        customArray.add(50);
-        customArray.add(2, 80);
-        customArray.addToFront(90);
+        for (int i = 0; i < 500; i++) {
+            customArray1.add(rd.nextInt(1000));
+            customArray2.add(rd.nextInt(500));
+        }
 
-        System.out.println(customArray);
+        System.out.println(customArray1);
+        System.out.println();
+        System.out.println(customArray2);
 
-        System.out.println("Removed index: " + customArray.delete(50));
-        System.out.println("Removed element: " + customArray.deleteByIndex(2));
-        System.out.println(customArray);
-        System.out.println("Length of list: " + customArray.length());
-        System.out.println("Index of 30 is: " + customArray.indexOf(30));
-        customArray.bubbleSort();
-        System.out.println(customArray);
+
+        long currentTime = System.currentTimeMillis();
+        customArray1.insertionSort();
+        long delta = System.currentTimeMillis() - currentTime;
+        System.out.println();
+        System.out.println(customArray1);
+        System.out.println(delta);
+
+
+
+        currentTime = System.currentTimeMillis();
+        customArray2.bubbleSort();
+        delta = System.currentTimeMillis() - currentTime;
+        System.out.println();
+        System.out.println(customArray2);
+        System.out.println(delta);
+
+
+
+//        customArray.add(10);
+//        customArray.add(20);
+//        customArray.add(0, 30);
+//        customArray.add(40);
+//        customArray.add(50);
+//        customArray.add(2, 80);
+//        customArray.addToFront(90);
+
+//        System.out.println(customArray);
+//
+//        System.out.println("Removed index: " + customArray.delete(50));
+//        System.out.println("Removed element: " + customArray.deleteByIndex(2));
+//        System.out.println(customArray);
+//        System.out.println("Length of list: " + customArray.length());
+//        System.out.println("Index of 30 is: " + customArray.indexOf(30));
+//        System.out.println(customArray);
+//
+//        long currentTime = System.currentTimeMillis();
+//        customArray.insertionSort();
+//        long delta = System.currentTimeMillis() - currentTime;
+//        System.out.println(delta);
+//
+//        System.out.println(customArray);
 
 
     }
