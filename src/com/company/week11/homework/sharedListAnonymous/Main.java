b322f95e1396ac9b29b3c38aa94abb58e9608cfa
Ruzanna Zohrabyan <ruzanna83@gmail.com>
2021-09-28 21:19:11 +0400

    Week 11 Changing HomeWork\SharedListAnonymous

--- src/com/company/week11/homework/sharedListAnonymous/Main.java
+++ src/com/company/week11/homework/sharedListAnonymous/Main.java
@@ -3,21 +3,27 @@ package com.company.week11.homework.sharedListAnonymous;
 import java.util.*;
 
 public class Main {
+    private List<Integer> sharedList = new ArrayList<>();
+
     public static void main(String[] args) throws InterruptedException {
-        List<Integer> sharedList = new ArrayList<>();
+        Main main = new Main();
         List<Thread> threadList = new ArrayList<>();
         for (int i = 0; i < 100; i++) {
             threadList.add(new Thread(() -> {
-                for (int j = 1; j <= 500 ; j++) {
-                    sharedList.add(j);
+                for (int j = 1; j <= 500; j++) {
+                    main.addToList(j);
                 }
             }));
         }
 
-        for (Thread thread : threadList){
+        for (Thread thread : threadList) {
             thread.start();
             thread.join();
         }
-        System.out.println(sharedList.size());
+        System.out.println(main.sharedList.size());
+    }
+
+    public synchronized void addToList(int item) {
+        sharedList.add(item);
     }
 }
